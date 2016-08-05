1c94e0c0a2921ed9b60ca4d0221a4545b94fd3dc
Qaiser Abbasi <Qaiser.Abbasi@codecentric.de>
2016-08-05 14:36:10 +0200

    Second test written

--- src/test/scala/de/codecentric/lunchbot/actors/MessagesFromSlackReceiverTest.scala
+++ src/test/scala/de/codecentric/lunchbot/actors/MessagesFromSlackReceiverTest.scala
@@ -11,20 +11,30 @@ import scala.concurrent.duration._
 
 class MessagesFromSlackReceiverTest extends TestKit(ActorSystem("test")) with FlatSpecLike with Matchers {
 
+  val receiversUser = User("1", "lunchbot", None, Some(true))
+  val defaultChat = "default chat channel"
+  val messageSendingUser = User("2", "Message Sender Dude", None, None)
+
   "A Receiver Actor" should "receive Messages from default chat channel" in {
-    val receiversUser = User("1", "lunchbot", None, Some(true))
-    val defaultChat = "default chat channel"
     val receiver = TestActorRef(MessagesFromSlackReceiver.props(defaultChat, receiversUser))
     receiver ! SlackEndpoint(testActor)
 
-    val messageSendingUser = User("2", "Message Sender Dude", None, None)
     receiver ! IncomingSlackMessage("message", defaultChat, "Hey @lunchbot!", "42", messageSendingUser.displayName)
 
     expectMsgPF(1.seconds) {
-      case TextMessage.Strict(text) => {
-        text.contains(messageSendingUser.displayName)
-      }
-    } shouldBe true
+      case TextMessage.Strict(text) if text.contains(messageSendingUser.displayName) => ()
+    }
+  }
+
+  it should "echo to incoming messages" in {
+    val receiver = TestActorRef(MessagesFromSlackReceiver.props(defaultChat, receiversUser))
+    receiver ! SlackEndpoint(testActor)
+
+    receiver ! IncomingSlackMessage("message", defaultChat, "woot", "42", messageSendingUser.displayName)
+
+    expectMsgPF(1.seconds) {
+      case TextMessage.Strict(text) if text.contains("ECHO: woot") => ()
+    }
   }
 
 }
